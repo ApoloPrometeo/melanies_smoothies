bcc4f0d52760e262c5eb1240bcdca6bb7ffcad70
ApoloPrometeo <165837553+ApoloPrometeo@users.noreply.github.com>
2024-04-02 14:35:24 -0600

    Update streamlit_app.py
    Fixing the line 13 "connection"

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write("""Choose the fruits you want in your custom Smoothie!""")
 name_on_order = st.text_input('Name on Snoothie')
 st.write('The name on your Smoothie will be', name_on_order)
 
-cnx = st.connectino("snowflake")
+cnx = st.connection("snowflake")
 session=cnx.session()
 
 ##option = st.selectbox(
