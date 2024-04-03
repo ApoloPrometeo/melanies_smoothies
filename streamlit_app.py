ca82628691b9f088edb9e7b299711edae6423bad
ApoloPrometeo <165837553+ApoloPrometeo@users.noreply.github.com>
2024-04-02 19:29:35 -0600

    Update streamlit_app.py
    importing  to_pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-import pandas
+import to_pandas
 st.title('My Parents New Healthy Diner')
 
 # Write directly to the app
